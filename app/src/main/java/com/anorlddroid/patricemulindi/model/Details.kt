dd9da815553ae4f42a9ea43b1492ed53e562b95b
anorld-droid <mulindipatrice00@gmail.com>
2022-10-27 09:45:42 +0300

    Make it parceble, to be able to pass it during navigation. Home to results.

--- app/src/main/java/com/anorlddroid/patricemulindi/model/Details.kt
+++ app/src/main/java/com/anorlddroid/patricemulindi/model/Details.kt
@@ -1,8 +1,12 @@
 package com.anorlddroid.patricemulindi.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Details (
     val name:String,
     val weight: Double,
     val height: Double,
     val gender: String
-        )
+        ): Parcelable
