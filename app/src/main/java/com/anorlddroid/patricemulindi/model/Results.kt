a005ab6e58655716bb2786f484d8d0d8580964ce
anorld-droid <mulindipatrice00@gmail.com>
2022-10-27 09:50:19 +0300

    No need to be parcelable.

--- app/src/main/java/com/anorlddroid/patricemulindi/model/Results.kt
+++ app/src/main/java/com/anorlddroid/patricemulindi/model/Results.kt
@@ -4,11 +4,11 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 
-@Parcelize
+
 data class Results(
     val name:String,
     val BMIIndex: List<String>,
     val PonderalIndex: String,
     val category: String,
     val categoryInfo: String
-): Parcelable
+)
